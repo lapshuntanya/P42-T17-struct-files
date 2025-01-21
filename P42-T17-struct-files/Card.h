e8b90a29d34122bc893ff3fae00765f22affe5b9
lapshuntanya <lapshun.tatiana@gmail.com>
2025-01-21 17:22:14 +0200

    Add project files.

---
+++ P42-T17-struct-files/Card.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct Card
+{
+	char name[50] = "undefined";	//Прізвище клієнта
+	unsigned long number = 0;		//Номер рахунку
+	double cash = 0;				//Сума коштів на рахунку
+
+	void createCard() {
+		cout << "Input your name: ";
+		cin.getline(name, 50); //Зчитування рядку з пробілами
+		number = rand() * 100'000 + rand();
+		cout << "Topup your cash (UAH): ";
+		cin >> cash;
+	}
+
+	void showCard() {
+		cout << "+++++++++++++++++++++++++++++++++++++\n";
+		cout << "| Client: " << name << endl;
+		cout << "| Card:   " << number << endl;
+		cout << "| Cash:   " << cash << " UAH" << endl;
+		cout << "+++++++++++++++++++++++++++++++++++++\n";
+	}
+};
---
+++ P42-T17-struct-files/P42-T17-struct-files.cpp
@@ -0,0 +1,11 @@
+#include "Card.h"
+
+int main()
+{
+    srand(time(0));
+
+    Card Valeria;
+    Valeria.createCard();
+    Valeria.showCard();
+
+}
